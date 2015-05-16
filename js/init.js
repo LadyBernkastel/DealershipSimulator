ab5b3561401362907f5c553f0d8d3a0408d9af49
LadyBernkastel <Rachael@bernkastel.co.uk>
2015-05-16 15:50:09 +0100

    Figruing out read operations from Firebase.

--- js/init.js
+++ js/init.js
@@ -3,11 +3,13 @@ var firebase = new Firebase("https://blazing-torch-6209.firebaseio.com/");
 $(document).ready(function() {
     $("#save-button").click(setFirebase());
     $("#push-button").click(pushFirebase());
+    $("#read-button").click(readFirebase());
 });
 
 function setFirebase() {
     firebase.set( {
-        connectivityTest: "true!"
+        make: "Volkswagen",
+        model: "up"
     });
 }
 
@@ -15,6 +17,18 @@ function pushFirebase() {
     var pushRef = firebase.child("cars");
     pushRef.push( {
         make: "Volkswagen",
-        model: "ups"
+        model: "up"
+    });
+}
+
+function readFirebase() {
+    var ref = new Firebase("https://blazing-torch-6209.firebaseio.com/cars");
+    ref.on("value", function(snapshot) {
+      snapshot.forEach(function(car) {
+        $("#read-content").html("Make: " + car.val().make + " Model:" + car.val().model);
+        return true;
+      })
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
     });
 }
